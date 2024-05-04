f7a9ccc45f33726d82c75f4a77b3d453d23d2d22
RDDcat <yukmaro@gmail.com>
2024-05-04 16:52:58 +0900

    [ADD] tailwind, vuex 추가

---
+++ src/store.js
@@ -0,0 +1,20 @@
+import Vuex from 'vuex';
+import VuexPersistence from 'vuex-persist';
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+  modules: [],
+});
+
+// 작성한 모듈을 가져오기
+import cacheStore from '@/store/cacheStore.js';
+import modalStore from '@/store/modalStore.js';
+const store = new Vuex.Store({
+  
+  modules: {
+    cacheStore,
+    modalStore,
+  },
+  plugins: [vuexLocal.plugin],
+});
+export default store;
---
+++ src/store/modalStore.js
@@ -0,0 +1,23 @@
+// Vuex 스토어를 정의하여 모달과 관련된 상태를 관리
+const modalStore = {
+  state: {
+    
+  },
+
+  getters: {
+
+  },
+  
+  mutations: {
+    CloseAll: (state) => {
+
+    },
+  },
+
+  actions: {
+
+  }
+};
+
+// modalStore를 내보냄
+export default modalStore;
