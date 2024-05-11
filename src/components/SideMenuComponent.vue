74ddc407ad455728aa281e82a6f3151673e09be4
RDDcat <yukmaro@gmail.com>
2024-05-11 19:23:10 +0900

    [UPDATE] 이미지 에셋 위치 수정 및 사이드 메뉴 아이콘 추가

--- src/components/SideMenuComponent.vue
+++ src/components/SideMenuComponent.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    sample page
+  <div class="w-20 h-full">
+    <!-- 로고 -->
+
+    <!-- 아이콘 리스트 -->
+    <div class="w-full">
+      <!-- 아이콘 컨테이너 -->
+      <div class="mx-auto my-2 p-1 w-12 h-12" v-for="icon, index in icon_list" :key="index">
+        <img class="w-full h-full object-cover" :src="icon">
+      </div>
+
+    </div>
   </div>
 </template>
 
@@ -11,7 +20,7 @@ export default {
   },
   data(){
       return {
-
+        icon_list: ['./assets/logo.png', './assets/emo-2.png', './assets/emo-3.png'],
       }
   },
   methods: {
