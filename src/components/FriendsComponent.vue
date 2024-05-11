44f4659cb967bb9811a1cc07641bee9bc5ea0d3e
RDDcat <yukmaro@gmail.com>
2024-05-11 20:41:46 +0900

    [UPDATE] 친구목록 css 색상변경

--- src/components/FriendsComponent.vue
+++ src/components/FriendsComponent.vue
@@ -5,7 +5,7 @@
       친구
     </div>
     <!-- 친구 목록 -->
-    <div class="flex flex-col w-96 overflow-x-hidden overflow-y-auto">
+    <div class="flex flex-col w-96 overflow-hidden overflow-y-auto">
       <!-- 친구 컴포넌트 -->
       <div class="flex w-full h-14 px-4 my-3">
         <!-- 이미지 -->
@@ -19,14 +19,14 @@
             김성준
           </div>
           <!-- 최근 메시지 -->
-          <div class="text-sm pl-2 my-auto">
+          <div class="text-sm pl-2 my-auto text-gray-400">
             뭐하누~!!!!!!!!!!
           </div>
         </div>
         <!-- 시간과 메시지 알림 -->
         <div class="flex flex-col w-auto h-full py-1 ml-auto mr-4">
           <!-- 시간 -->
-          <div class="font-semibold text-base pl-2 my-auto">
+          <div class="font-semibold text-base pl-2 my-auto text-gray-400">
             5.11
           </div>
           <!-- 알림 -->
